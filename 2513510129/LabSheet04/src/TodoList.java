a902276ef800fdef524050d407c43afc4de3a1db
Puth54 <151760970+Puth54@users.noreply.github.com>
2026-07-26 21:31:12 +0700

    Enhance TodoList with removal and formatted output
    Updated the TodoList class to include additional functionality for removing items and improved output formatting.

--- 2513510129/LabSheet04/src/TodoList.java
+++ 2513510129/LabSheet04/src/TodoList.java
@@ -1,27 +1,39 @@
-
 public class TodoList {
-	public static void main(String[] args) {
-		DoublyLinkedList todoList = new DoublyLinkedList();
-		
-		todoList.insert("Finish Homework");
-		System.out.println(todoList.traversal());
-		
-		todoList.insert("Laundry");
-		todoList.insert("Group Meeting");
-		System.out.println(todoList.traversal());
-		
-		//Exercise no.2.
-		todoList.insert(0, "Submit Report");
-		System.out.println(todoList.traversal());
-		
-		//Exercise no.2.2
-		todoList.insert(2, "Buy Food");
-		System.out.println(todoList.traversal());
-		
-		//Exercise no.2.3
-		todoList.insert("Go to Gym");
-		System.out.println(todoList.traversal());
-	}
-
-
+    public static void main(String[] args) {
+        DoublyLinkedList todoList = new DoublyLinkedList();
+        
+        todoList.insert("Finish Homework");
+        todoList.insert("Laundry");
+        todoList.insert("Group Meeting");
+        System.out.println("To-Do List = " + todoList.traversal());
+        
+        // no. 2.1)
+        todoList.insert(0, "Submit Report");
+        System.out.println("To-Do List = " + todoList.traversal());
+        
+        // no. 2.2) 
+        todoList.insert(2, "Buy Food");
+        System.out.println("To-Do List = " + todoList.traversal());
+        
+        // no. 2.3) 
+        todoList.insert("Go to Gym");
+        System.out.println("To-Do List = " + todoList.traversal());
+        
+        // --- no. 4 ---
+        // Now List: [Submit Report, Finish Homework, Buy Food, Laundry, Group Meeting, Go to Gym]
+        // no. 4.1) 
+        todoList.remove(); 
+        
+        // no. 4.2) 
+        todoList.remove(3); 
+        
+        // no. 4.3) 
+        todoList.remove(0); 
+        
+        // no. 4.4)
+        System.out.println("To-Do List = " + todoList.traversal());
+        
+        // no. 5.2)
+        System.out.println("Backward Traversal = " + todoList.backwardTraversal());
+    }
 }
